243bdc54b9792c4a9cffb85a26f3675ebdc529f0
Husam0504 <husam.mustafa@aol.com>
2023-08-10 20:22:15 -0700

    first code

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - comment required
+ *return : zero always (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
